c26bed5bb7b9da532b9304d2eacb9b4957ac3403
Emin <sambufer200@gmail.com>
2020-04-23 17:14:11 +0300

    fixing .gitignore

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course, Module, Subject
+
+
+class ModuleInline(object):
+    pass
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subject', 'created']
+    search_fields = ['name', 'view']
+    list_filter = ['data_created', 'subject']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ModuleInline]
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
