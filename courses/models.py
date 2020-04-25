2b695f8941a64ae3f25d4a81ab15174efbe97d80
Emin <sambufer200@gmail.com>
2020-04-25 17:52:29 +0300

    added new modeles in courses

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils import timezone
 
 
 class Subject(models.Model):
@@ -41,3 +44,32 @@ class Module(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE,
+                               verbose_name="Содержимое модуля")
+    type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    obj_id = models.PositiveIntegerField()
+    item = GenericForeignKey('type', 'obj_id')
+
+
+class AbstractContent(models.Model):
+    name = models.CharField(max_length=150)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Text(AbstractContent):
+    words = models.TextField(verbose_name="Текст")
+
+
+class SortingContent(AbstractContent):
+    class Meta:
+        proxy = True
+        ordering = ['date_created']
+
+    def time(self):
+        return timezone.now() - self.date_created
